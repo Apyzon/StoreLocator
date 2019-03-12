c7df6e5a8c117a6c2fcf42370a54d61c3329c782
Apazon <raymond.olani@gmail.com>
2019-03-12 19:08:14 +0000

    Build 1.2 - Fragments added and interchangeable for customer page

--- app/src/main/java/com/o/storelocator/customerHome.java
+++ app/src/main/java/com/o/storelocator/customerHome.java
@@ -3,13 +3,17 @@ package com.o.storelocator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+//import android.app.Fragment;
+
 public class customerHome extends AppCompatActivity {
 
     private TextView mTextMessage;
+    FragmentTransaction transaction;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +22,22 @@ public class customerHome extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.cus_title_1);
+                    //mTextMessage.setText(R.string.cus_title_1);
+                    transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.add(R.id.frameLayout, new Map());
+                    transaction.commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.cus_title_2);
+                    //mTextMessage.setText(R.string.cus_title_2);
+                    transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.add(R.id.frameLayout, new Stores());
+                    transaction.commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.cus_title_3);
+                    //mTextMessage.setText(R.string.cus_title_3);
+                    transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.add(R.id.frameLayout, new Search());
+                    transaction.commit();
                     return true;
             }
             return false;
@@ -40,5 +53,6 @@ public class customerHome extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }
+
+
